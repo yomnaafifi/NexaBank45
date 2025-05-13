f74d3b4f0909136822fe7fc77ee363995cd5a392
yomnaafifi <yomnaaafifi162@gmail.com>
2025-05-13 18:52:13 +0300

    edit Loaders

--- ETL/Loaders.py
+++ ETL/Loaders.py
@@ -23,27 +23,38 @@ class LocalParquetWriter(BaseWriter):
 
 class BaseLoader(ABC):
     """Base class for loaders that handle the loading of dataframes to different destinations."""
-    def __init__(self, dataframe: pd.DataFrame, local_path: str, loading_path: str):
+    def __init__(self, dataframe: pd.DataFrame, loading_path:str, file_name:str):
         self.dataframe = dataframe
-        self.local_path = local_path
         self.loading_path = loading_path
+        self.file_name = file_name
+        self.full_path = os.path.join(self.loading_path, self.file_name) 
 
     @abstractmethod
     def load(self) -> bool:
         pass
+
+class LocalLoader(BaseLoader):
+    """Load dataframe to local file system using parquet writer"""
+    def load(self) -> bool:
+        try:
+            writer = LocalParquetWriter(self.dataframe, self.full_path)
+            writer.write()
+            return True
+        except Exception as e:
+            print(f"[ERROR] Failed to load to local: {e}")
+            return False
 
 
 class HdfsLoader(BaseLoader):
     """Load dataframe to HDFS using parquet writer"""
     def load(self) -> bool:
         try:
-            writer = LocalParquetWriter(self.dataframe, self.local_path)
-            local_file = writer.write()
+            LocalLoader(self.dataframe, "tmp", self.file_name).load()
 
-            subprocess.run(["hdfs", "dfs", "-mkdir", "-p", os.path.dirname(self.loading_path)], check=True)
-            subprocess.run(["hdfs", "dfs", "-put", "-f", local_file, self.loading_path], check=True)
+            subprocess.run(["hdfs", "dfs", "-mkdir", "-p", self.loading_path], check=True)
+            subprocess.run(["hdfs", "dfs", "-put", "-f", self.full_path, self.loading_path], check=True)
 
-            os.remove(local_file)
+            os.remove(self.full_path)
             return True
         
         except Exception as e:
